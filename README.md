a4466024ed2dffb70da12a6f742498061e65d950
umabubakar16 <40616513+umabubakar16@users.noreply.github.com>
2018-06-26 18:12:48 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# my-udacity-pixel-art
+# Pixel-Art-Maker-project
+Udacity Pixel Art Maker project using basic HTML, CSS, JQuery to complete this project.
---
+++ designs.js
@@ -0,0 +1,45 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid(width, height) {
+	let pixelCanvas, grid;
+	pixelCanvas = $('#pixel_canvas');
+	if (pixelCanvas.children().length) {
+		pixelCanvas.empty();
+	}
+	for(let i = 0; i < height; i++) {
+		grid += "<tr>";
+		for (let j = 0; j < width; j++) {
+			grid += "<td></td>";
+		}
+		grid += "</tr>";
+	}
+	pixelCanvas.html(grid);
+	pixelCanvas.css("background-color", "#ffffff");
+};
+
+$(document).ready(function(){
+	$('#submit-btn').click(function(evt) {
+		let height, width;
+		width = $('#input_width').val();
+		height = $('#input_height').val();
+		evt.preventDefault();
+		makeGrid(width, height);
+	});
+
+	$('#pixel_canvas').click(function(evt) {
+		let color = $('#colorPicker').val();
+		$(evt.target).css('background-color', color);
+	});
+});
+
+
+
+
+
+
+
+
+
